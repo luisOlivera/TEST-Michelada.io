b41b8c5d0965f30909cc80c45b53411bb2dc2751
luisOlivera <olis_55@hotmail.com>
2017-08-12 08:35:07 -0500

    Creación de la BD

---
+++ Server/Script-DB.sql
@@ -0,0 +1,15 @@
+﻿create schema michelada_TEST authorization postgres;
+set search_path to michelada_TEST;
+
+create table marcas(
+    id_marca bigint primary key,
+    nombre varchar(100) not null
+);
+
+create table productos(
+    id_producto bigint primary key,
+    nombre varchar(100) not null,
+    id_marca bigint references marcas(id_marca),
+    descripcion varchar(200),
+    precio numeric not null check (precio > 0 and precio <= 180)
+);
